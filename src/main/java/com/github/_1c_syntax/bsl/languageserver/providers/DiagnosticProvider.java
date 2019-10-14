5f04be4b2ec00b65330c4c1e0fc131c4d50d7d73
Tymko Oleg <olegtymko@yandex.ru>
2019-10-14 23:35:12 +0700

    подход #2

--- src/main/java/com/github/_1c_syntax/bsl/languageserver/providers/DiagnosticProvider.java
+++ src/main/java/com/github/_1c_syntax/bsl/languageserver/providers/DiagnosticProvider.java
@@ -21,17 +21,11 @@
  */
 package com.github._1c_syntax.bsl.languageserver.providers;
 
-import com.github._1c_syntax.bsl.languageserver.context.ServerContext;
-import com.github._1c_syntax.bsl.languageserver.context.computer.DiagnosticIgnoranceComputer;
-import com.google.common.annotations.VisibleForTesting;
-import org.apache.commons.io.IOUtils;
-import org.eclipse.lsp4j.Diagnostic;
-import org.eclipse.lsp4j.PublishDiagnosticsParams;
-import org.eclipse.lsp4j.jsonrpc.messages.Either;
-import org.eclipse.lsp4j.services.LanguageClient;
 import com.github._1c_syntax.bsl.languageserver.configuration.LanguageServerConfiguration;
 import com.github._1c_syntax.bsl.languageserver.context.DocumentContext;
 import com.github._1c_syntax.bsl.languageserver.context.FileType;
+import com.github._1c_syntax.bsl.languageserver.context.ServerContext;
+import com.github._1c_syntax.bsl.languageserver.context.computer.DiagnosticIgnoranceComputer;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.BSLDiagnostic;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticMetadata;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticParameter;
@@ -39,6 +33,12 @@ import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticS
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticSeverity;
 import com.github._1c_syntax.bsl.languageserver.diagnostics.metadata.DiagnosticType;
 import com.github._1c_syntax.bsl.languageserver.utils.UTF8Control;
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.commons.io.IOUtils;
+import org.eclipse.lsp4j.Diagnostic;
+import org.eclipse.lsp4j.PublishDiagnosticsParams;
+import org.eclipse.lsp4j.jsonrpc.messages.Either;
+import org.eclipse.lsp4j.services.LanguageClient;
 import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
@@ -50,7 +50,6 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.HashMap;
@@ -86,13 +85,13 @@ public final class DiagnosticProvider {
   private final Map<String, Set<Diagnostic>> computedDiagnostics;
 
   public DiagnosticProvider() {
-    this(LanguageServerConfiguration.create());
+    this(LanguageServerConfiguration.create(), new ServerContext());
   }
 
-  public DiagnosticProvider(LanguageServerConfiguration configuration) {
+  public DiagnosticProvider(LanguageServerConfiguration configuration, ServerContext context) {
     this.configuration = configuration;
     computedDiagnostics = new HashMap<>();
-    context = new ServerContext();
+    this.context = context;
   }
 
   public void computeAndPublishDiagnostics(LanguageClient client, DocumentContext documentContext) {
@@ -122,6 +121,10 @@ public final class DiagnosticProvider {
     computedDiagnostics.put(documentContext.getUri(), new LinkedHashSet<>(diagnostics));
 
     return diagnostics;
+  }
+
+  public ServerContext getContext() {
+    return context;
   }
 
   public Set<Diagnostic> getComputedDiagnostics(DocumentContext documentContext) {
@@ -424,11 +427,4 @@ public final class DiagnosticProvider {
       || enabledDirectly;
   }
 
-  public void setSrcDirForServerContext(Path srvDir) {
-    context.setPathToConfigurationMetadata(srvDir);
-  }
-
-  public ServerContext getContext() {
-    return context;
-  }
 }
